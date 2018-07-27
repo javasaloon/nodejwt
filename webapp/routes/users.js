9498246dd01314d1dcc2736b9809c84c8ede4948
Jacky Cheng <jacky.cheng@sap.com>
2018-07-27 16:19:44 +0800

    add express-jwt

--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -5,20 +5,31 @@ router.use(bodyParser.json());       // to support JSON-encoded bodies
 router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+var ejwt = require('express-jwt');
 
 var jwt = require('jsonwebtoken');
 const auth = require('../libs/auth');
 
-/* GET users listing. */
+/* unprotected api GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 
+/* protected api, simply authenticate before enter this api */
+router.get('/protected',
+  ejwt({ secret: auth.secret }),
+  function (req, res) {
+
+    res.sendStatus(200);
+  });
+
+/* protected api GET one user by id. You can do more logic with the decoded information such as authorization*/
 router.get('/:id', function (req, res) {
   try {
     var decoded = jwt.verify(req.get("Authorization").substring(7), auth.secret);
     console.log(decoded);
+
     res.status(200)
       .json(
         decoded
@@ -36,6 +47,8 @@ router.get('/:id', function (req, res) {
 });
 
 
+/** create token which is used to call other protected apis */
+
 router.post('/login', (req, res) => {
   console.log("body: " + JSON.stringify(req.body));
   let user = { "email": req.body.email };
@@ -43,8 +56,8 @@ router.post('/login', (req, res) => {
     .json({
       success: true,
       token: auth.createJWTToken({
-        sessionData: user,
-        maxAge: 3600
+        sessionData: user,//whatever data you want to encode into the session
+        maxAge: 3600 // expiration time in seconds
       })
     });
 });
