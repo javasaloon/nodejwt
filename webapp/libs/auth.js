6ab546c13277f469467b01e4ef4d616a39c770ab
Jacky Cheng <jacky.cheng@sap.com>
2018-07-27 15:47:03 +0800

    done

--- webapp/libs/auth.js
+++ webapp/libs/auth.js
@@ -1,53 +1,24 @@
 var jwt = require('jsonwebtoken');
-var _ = require('lodash');
-
 
 let secret = process.env.JWT_SECRET || "your_secret";
+module.exports.secret = secret;
 
-module.exports.verifyJWTToken = function (token) 
-{
-  return new Promise((resolve, reject) =>
-  {
-    jwt.verify(token, secret, (err, decodedToken) => 
-    {
-      if (err || !decodedToken)
-      {
-        return reject(err)
-      }
-
-      resolve(decodedToken)
-    })
-  })
-}
-
-module.exports.createJWTToken = function (details)
-{
-  if (typeof details !== 'object')
-  {
+module.exports.createJWTToken = function (details) {
+  console.log(details);
+  if (typeof details !== 'object') {
     details = {}
   }
 
-  if (!details.maxAge || typeof details.maxAge !== 'number')
-  {
+  if (!details.maxAge || typeof details.maxAge !== 'number') {
     details.maxAge = 3600
   }
 
-  details.sessionData = _.reduce(details.sessionData || {}, (memo, val, key) =>
-  {
-    if (typeof val !== "function" && key !== "password")
-    {
-      memo[key] = val
-    }
-    return memo
-  }, {})
-
   let token = jwt.sign({
-     data: details.sessionData
-    }, secret, {
+    data: details.sessionData
+  }, secret, {
       expiresIn: details.maxAge,
       algorithm: 'HS256'
-  })
+    })
 
   return token
 };
- 
--- webapp/routes/users.js
+++ webapp/routes/users.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser')
+router.use(bodyParser.json());       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
+var jwt = require('jsonwebtoken');
 const auth = require('../libs/auth');
 
 /* GET users listing. */
@@ -8,22 +14,20 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/:id', function (req, res, next) {
 
-  auth.verifyJWTToken(req, res, next);
+router.get('/:id', function (req, res) {
+  console.log(jwt.verify(req.get("Authorization").substring(7), auth.secret));
 
   res.status(200)
-    .json({
-      success: true
-    });
+    .json(
+      jwt.verify(req.get("Authorization").substring(7), auth.secret)
+    )
 });
 
+
 router.post('/login', (req, res) => {
-  let { email, password } = req.body
-
-  let user = { "email": email };
-
-
+  console.log("body: " + JSON.stringify(req.body));
+  let user = { "email": req.body.email };
   res.status(200)
     .json({
       success: true,
